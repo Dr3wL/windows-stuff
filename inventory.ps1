b49e74586fe924f277cf87455b2030e4363ef5b4
Dr3wL <93017330+Dr3wL@users.noreply.github.com>
2024-10-08 23:20:57 -0400

    Update inventory.ps1

--- inventory.ps1
+++ inventory.ps1
@@ -10,31 +10,30 @@ $computerInfo = @()
 # Loop through each computer
 foreach ($computer in $computers) {
     if ($computer.OperatingSystem -like "*Windows*") {
-        # Get computer details
-        $hostName = $computer.Name
-        $ipv4Address = $computer.IPv4Address
-        $ipv6Address = $computer.IPv6Address
-        $macAddress = $computer.DNSHostName
+        # Get IP address info using Invoke-Command
+        $ipInfo = Invoke-Command -ComputerName $computer.Name -Credential $credentials -ScriptBlock {
+            $activeNic = Get-NetAdapter | Select-Object -ExpandProperty IfIndex
+            [ordered]@{
+                "Hostname"        = $env:COMPUTERNAME
+                "IPv4 Address"    = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.ifIndex -eq $activeNic} | Select-Object -ExpandProperty IPAddress)
+                "IPv6 Address"    = (Get-NetIPAddress -AddressFamily IPv6 | Where-Object { $_.ifIndex -eq $activeNic} | Select-Object -ExpandProperty IPAddress)
+                "MAC Address"     = (Get-NetAdapter | Select-Object -ExpandProperty MacAddress)
+            }
+        }
 
         # Get operating system details
         $operatingSystem = $computer.OperatingSystem
 
-        # Create a CIM session using the provided credentials
-        $session = New-CimSession -ComputerName $hostName -Credential $credentials -ErrorAction SilentlyContinue
-
-        # Get services and versions (using Get-CimInstance with the session)
-        $services = Get-CimInstance -ClassName Win32_Service -CimSession $session -ErrorAction SilentlyContinue | 
+        # Get services and versions (using Get-WmiObject)
+        $services = Get-WmiObject -Class Win32_Service -ComputerName $computer.Name -ErrorAction SilentlyContinue -Credential $credentials | 
             Select-Object Name, DisplayName, StartMode, State, PathName
-
-        # Remove the session
-        Remove-CimSession -CimSession $session
 
         # Create a custom object to store computer information
         $info = [PSCustomObject]@{
-            "Host Name"            = $hostName
-            "IPv4 Network Address" = $ipv4Address
-            "IPv6 Network Address" = $ipv6Address
-            "Mac"                  = $macAddress
+            "Host Name"            = $ipInfo.Hostname
+            "IPv4 Network Address" = $ipInfo."IPv4 Address"
+            "IPv6 Network Address" = $ipInfo."IPv6 Address"
+            "Mac"                  = $ipInfo."MAC Address"
             "Operating System"     = $operatingSystem
             "Services and Versions" = ($services | Out-String).Trim()
         }
